7a40ffb8bcfd0f259c0d26f730ff94df4e49a61e
Aamir Mohammed <maamir@mail.uni-paderborn.de>
2020-05-31 15:58:43 +0200

    Model and datasetUri reading successfully from postman

--- src/main/java/org/diceresearch/civetWebService/Controller.java
+++ src/main/java/org/diceresearch/civetWebService/Controller.java
@@ -1,13 +1,28 @@
 package org.diceresearch.civetWebService;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class Controller {
-	@PostMapping("/example")
-	  String Example(@RequestBody String example) {
-	    return "Hello "+ example;
-	  }
+	
+	@PostMapping("/uploadFile")
+    public int uploadFile(@RequestParam("file") MultipartFile file, @RequestParam String dataset) throws IOException {
+		Model model = ModelFactory.createDefaultModel();
+		model.read( new ByteArrayInputStream( file.getBytes() ), null, "TTL" );
+		String datasetUri = new String(dataset.getBytes(), StandardCharsets.UTF_8);
+		return compute(model, datasetUri);
+	}
+	
+	public int compute(Model model, String datasetUri) {
+		return 5;
+	}
 }
