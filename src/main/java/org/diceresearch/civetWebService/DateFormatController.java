df5ec712219af878311a3f646e7cffa7c3c9475a
Aamir Mohammed <maamir@mail.uni-paderborn.de>
2020-06-08 13:03:29 +0200

    exisiting measurements method called and comments added

--- src/main/java/org/diceresearch/civetWebService/DateFormatController.java
+++ src/main/java/org/diceresearch/civetWebService/DateFormatController.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.dice_research.opal.civet.Civet;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +23,21 @@ public class DateFormatController {
 
 	@PostMapping("/uploadFile")
 	public int uploadFile(@RequestParam("file") MultipartFile file, @RequestParam String dataSet) throws Exception {
+
+		/* Creating a Default model to Load the turtle file */
 		Model model = ModelFactory.createDefaultModel();
+
+		/* Reading the turtle file */
 		model.read(new ByteArrayInputStream(file.getBytes()), null, "TTL");
+
+		/* Reading the datasetUri */
 		String datasetUri = new String(dataSet.getBytes(), StandardCharsets.UTF_8);
+
+		/* If existing measurements should be removed */
+		Civet civet = new Civet();
+		civet.setRemoveMeasurements(true);
+
+		/* Compute model and datasetUri */
 		DateFormatMetric metric = new DateFormatMetric();
 		return metric.compute(model, datasetUri);
 	}
